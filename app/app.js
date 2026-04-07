645f32caedfaa605d2fd15192da2500834d6e94c
AliYasser2003 <aliyasserali0111@gmail.com>
2026-04-07 18:48:13 +0200

    connect github to eks

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Project 4 CI/CD is working \n');
+  res.end('Project 4 CI/CD is working \n new version with EKS');
 });
 
 server.listen(4000, () => {
