63d875d55f426341de92ae1246f1346adec8734a
AliYasser2003 <aliyasserali0111@gmail.com>
2026-04-08 12:32:44 +0200

    fix crash in app.js

--- app/app.js
+++ app/app.js
@@ -1,9 +1,9 @@
-explorer.exe .const http = require('http');
+const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('TEST VERSION AUTO');
+  res.end('TESTING VERSION AUTO');
 });
 
 server.listen(4000, () => {
